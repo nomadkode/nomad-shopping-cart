e6be90a9f36d95fcab91add96da83645c83a16ff
nomadkode <setiawanramadyan@gmail.com>
2021-01-23 15:17:41 +0700

    add Button.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Header = ({ title }) => {
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="Add" />
     </header>
   );
 };
@@ -15,11 +17,5 @@ Header.defaultProps = {
 Header.propTypes = {
   title: PropTypes.string,
 };
-
-// CSS in JS
-// const headingStyle = {
-//   color: 'red',
-//   backgroundColor: 'black',
-// };
 
 export default Header;
