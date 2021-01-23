049315cda710efe75cf279d83b5eaa021c4c4d84
nomadkode <setiawanramadyan@gmail.com>
2021-01-23 14:58:44 +0700

    add index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-const Header = () => {
+import PropTypes from 'prop-types';
+
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>Tracker App</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
+
+Header.defaultProps = {
+  title: 'Tracker App',
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
+// };
 
 export default Header;
